50d95303d3056f8ef912b95f114a5d73ffda57ce
kevham78 <45171194+kevham78@users.noreply.github.com>
2021-11-08 19:28:40 -0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace HHPool.App
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+    }
+}
